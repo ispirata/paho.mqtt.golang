a1cc349bb6e1ef6351bb5d15815ab04803e7b5a7
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2022-01-21 16:49:36 +0100

    Replace github.com/eclipse/paho.mqtt.golang with github.com/ispirata/paho.mqtt.golang
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- fvt_store_test.go
+++ fvt_store_test.go
@@ -24,7 +24,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/eclipse/paho.mqtt.golang/packets"
+	"github.com/ispirata/paho.mqtt.golang/packets"
 )
 
 /**********************************************
--- unit_router_test.go
+++ unit_router_test.go
@@ -22,7 +22,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/eclipse/paho.mqtt.golang/packets"
+	"github.com/ispirata/paho.mqtt.golang/packets"
 )
 
 func Test_newRouter(t *testing.T) {
